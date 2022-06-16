36cc6f8d2ed6d92a92eecf8dbc0309287052dfca
nergir <nerijus.girskis@ngsolution.eu>
2022-06-17 01:12:24 +0300

    first commit

---
+++ template.py
@@ -0,0 +1,137 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+import re
+from cgi import escape #urllib.quote
+blocks={}
+
+def load_template(template=None,block=False,variable=False,fix=False,form=False,overwrite=False):
+	if template:
+		blocks['']=template
+		if overwrite:fix=True
+		if fix:parse_fix()
+		if block:parse_block()
+		if variable:parse_variable(default=overwrite)
+		if form:parse_form()
+	return get_block('')
+
+def get_block(name):
+	if name in blocks:
+		return blocks[name]
+	return ''
+
+def set_block(name,value=None):
+	if value:
+		blocks[name]=value
+	else:
+		del blocks[name]
+
+def parse_fix(template=None):
+	if template==None:template=get_block('')
+	pattern='<!--\s*START\s*([\w\d_]+)\s-->((?:.|\s)*?)<!--\s*END\s*(\\1)\s-->'
+	while True:
+		tmp=re.sub(pattern,'<!-- \\1 \\2 \\1 -->',template)
+		if tmp==template:break
+		template=tmp
+	pattern='(<[^>]+)(value\s*=\s*(?:\w*|\'[^\']*\'|"[^"]*"))([^>]+)(name\s*=\s*(?:\w*|\'[^\']*\'|"[^"]*"))([^>]*>)'
+	template=re.sub(pattern,'\\1\\4\\3\\2\\5',template)
+	blocks['']=template
+	return template
+
+def parse_block(template=None):
+	if template==None:template=get_block('')
+	def block(mm):
+		pattern='<!--\s*([\w\d_]+)\s*((?:.|\s)*?)(\s+\\1)?\s*-->';
+		m=re.match(pattern,mm.group(0))
+		if m:
+			blocks[m.group(1)]=m.group(2)
+			if m.group(3):return '{%s}' % m.group(1)
+		return ''
+	pattern='<!\s*(?:--(?:[^\-]|[\r\n]|-[^\-])*--\s*)>'
+	p=re.compile(pattern)
+
+	while True:
+		new=p.sub(block,template)
+		if template==new:break
+		template=new
+	blocks['']=template
+	return template
+
+def parse_form(template=None,default=False):
+	if template==None:template=get_block('')
+	def form(m):
+		if m.group(1):
+			name,variable=m.group(1).split('=')
+			variable=variable.strip()
+		else:
+			variable='form'
+		blocks[variable]=m.group(0)
+		return '{%s}' % variable
+
+	pattern='<form\s*(name\s*=\s*(?:\w*|\'[^\']*\'|"[^"]*"))?[^>]*>((?:.|\s)*?)</form>';
+	template=re.sub(pattern,form,template)
+	blocks['']=template
+	return template
+
+def parse_variable(template=None,default=False):
+	if template==None:template=get_block('')
+	def attrs(m):
+		if m.group(2):
+			if default:
+				s=m.group(0)
+				name,variable=m.group(1).split('=')
+				variable="{%s}" % variable.strip()
+				if s.startswith(('textarea','select','iframe','div'),1):
+					i=m.end(0)-m.start(0)
+				else:
+					variable='value=\'%s\''% variable
+				return s[:m.start(2)-m.start(0)]+variable+s[m.end(2)-m.start(0):]
+			else:
+				return m.group(0)
+		else:
+			s=m.group(0)
+			name,variable=m.group(1).split('=')
+			variable="{%s}" % variable.strip()
+			if s.startswith(('textarea','select','iframe','div'),1):
+				i=m.end(0)-m.start(0)
+			else:
+				variable=' value=\'%s\''% variable
+				i=m.end(1)-m.start(0)
+			return s[:i]+variable+s[i:]
+		
+	pattern='<[^>]+(name\s*=\s*(?:[^ >]*|\'[^\']*\'|"[^"]*"))(?:[^>]+(value\s*=\s*(?:[^ >]*|\'[^\']*\'|"[^"]*")))?[^>]*>'
+	template=re.sub(pattern,attrs,template)
+	blocks['']=template
+	return template
+
+def assign_block(name,vars,debug=False):
+	return assign(get_block(name),vars,debug)
+
+def assign(template,vars,debug=False):
+	def dodos(template,vars):
+		offset=0
+		pattern='\{([^\s\{\}]+)\}'
+		p=re.compile(pattern)
+		m=p.search(template)
+		while m:
+			if m.group(1) in vars:
+				s=vars[m.group(1)]
+				if not m.group(1) in blocks:s=escape(s)
+				template=template[:m.start(0)]+s+template[m.end(0):]
+				offset=m.start(0)+len(s)
+			elif m.group(1) in blocks:
+				s=dodos(blocks[m.group(1)],vars)
+				template=template[:m.start(0)]+s+template[m.end(0):]
+				offset=m.start(0)+len(s)
+			else:
+				if debug:
+					offset=m.end(0)
+				else:
+					template=template[:m.start(0)]+template[m.end(0):]
+					offset=m.start(0)
+			m=p.search(template,offset)
+		return template
+
+	if type(vars)==dict:
+		return dodos(template,vars)
+	return '\n'.join([dodos(template,var) for var in vars])
---
+++ tplx.py
@@ -0,0 +1,208 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+from template import *
+from cgi import escape
+
+form="""
+<form name=User action=?f=user method=post enctype=multipart/form-data onSubmit=return(check(this)) lang='Ar iðsiøsti skelbimà?' onReset="return(confirm('Ar iðvalyti anketos laukus?'))">
+<p class=ln>Privilegija</p><select lang='0`Parinkite privilegija' name=Privilegija><option value=0><option value=1>Admin</option></select>
+<fieldset><legend onclick=toggle(this.nextSibling) style=cursor:pointer>Informacija</legend><div>
+<p class=ln>Vardas</p><input type=text name=Name style=text-transform:capitalize>
+<br><p class=ln>Pavardë</p><input type=text name=Surname style=text-transform:capitalize>
+<br><p class=ln>Gimtadienis</p><input type=text name=Gimtadienis> <input type=button title='Kalendorius' id=date class=sq onclick=showXY(event,'cldr') value=...>
+<br><p class=ln>Slapyvardis</p><input type=text name=Slapyvardis>
+<br><p class=ln>Slaptaþodis</p><input type=password name=Login maxLength=20>
+<!--human-->
+</div>
+</fieldset>
+<fieldset><legend onclick=toggle(this.nextSibling) style=cursor:pointer>Kontaktai</legend><div>
+<p class=ln>Telefonas</p><input type=text name=Telefonas maxLength=8>
+<br><p class=ln>Paðtas</p><input type=text name=Paðtas maxLength=20>
+</div>
+</fieldset>
+<fieldset><legend onclick=toggle(this.nextSibling) style=cursor:pointer>Adresas</legend><div>
+<p class=ln>Miestas</p><input type=text name=Miestas style=text-transform:capitalize>
+<br><p class=ln>Gatvë</p><input type=text name=Gatvë>
+<br><p class=ln>Namas</p><input type=text name=Namas>
+<br><p class=ln>Butas</p><input type=text name=Butas>
+</div>
+</fieldset>
+<fieldset><legend onclick=toggle(this.nextSibling) style=cursor:pointer>Extra</legend><div style=display:none>
+<p class=ln>Kaina</p><input type=text name=Kaina maxLength=20> <input type=button title='Kalkuliatorius' id=number class=sq onclick=showXY(event,'calc') value=...>
+<br><p class=ln>Spalva</p><input type=text name=Spalva maxLength=6 style=text-transform:uppercase onkeydown=keydown(event,'clrs') onkeyup=if((s=clrget(Title(this.value)))==this.value)return;this.value=s;color.style.backgroundColor=s> <input type=button title='Spalvø paletë' id=color class=sq onclick=showXY(event,'clrs') value=...>
+<br><p class=ln>Logo</p><input name=Logo lang='/^\d+(,\d+)*$/'> <input type=button class=sq value=... onclick=showXY(event,'upload',uploading,'Byla') title='Parinkti bylà'>
+<br><p class=ln>Foto</p><input name=Foto lang='/^\d+(,\d+)*$/'> <input type=button class=sq value=... onclick=showXY(event,'upload',uploading,'Byla') title='Parinkti bylà'>
+<br><p class=ln>CV</p><input name=CV lang='/^\d+(,\d+)*$/'>
+</div>
+</fieldset>
+<img src=img/1.ico title=Tinka onclick=dosubmit(this)>
+<img src=img/reset.ico title=Valyti onclick=doreset(this)>
+<img src=img/help.ico title=Pagalba onclick=doPagalba()>
+<img src=img/save.ico title=Atsiminti onclick=doForma(this,1)>
+<img src=img/open.ico title=Atverti onclick=doForma(this)>
+<img src=null.ico class='icon id' title=ID onclick=doID(this)>
+<input type=submit value=' ' Pateikti style=width:16;height:16;border:0>
+<input type=hidden name=ID>
+</form>
+<input value=321 name=demo>
+<input name=test value=123>
+"""
+line="""
+<html>
+<head>
+	<title>ngs — ng solution. For everyone</title>
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+<!--ona at-si-ti-ko -->
+<!--tbl
+	this is the
+	total
+	<!--tr
+		<!--this 
+		is another {sources}
+		this--><!--secondas 
+		
+		
+		secondas--> 
+	tr-->
+	first
+-->
+<!--content
+	hello
+	<!--subcontent
+		inside of hello1
+	subcontent-->
+	<!--subcontent
+		inside of hello2
+	subcontent-->
+	there
+-->
+<!-- 
+one toksdalykas 
+
+
+
+dar karteli
+padaryta
+
+-->
+</head>
+<body>
+This place for static block assign programically self
+<hr>
+{hello_world}
+<hr>
+<table>
+    <!-- START row -->
+        <tr>
+            <!-- START cell --><td>{td_value}</td><!-- END cell -->
+        </tr>
+    <!-- END row -->
+</table>
+<hr>
+<div>
+    BARS IN BERLIN:
+    <!-- START bar -->
+    <ul>
+        <li><strong>{name}</strong></li>
+        <li>{desc}</li>
+    </ul>
+    <!-- END bar -->
+</div>
+<hr>
+Coding PHP is <!-- START opt1 -->fun<!-- END opt1 --><!-- START opt2 -->boring<!-- END opt2 -->
+<hr>
+<!-- list {variable1} {variable2} list -->
+<hr>
+<div name=div></div>
+<hr>
+My invented code advantage:<xmp>For description changed {} to [], name to nane
+< !-- START blockname -- >...< !-- END blockname -- > => < !-- blockname ... blockname -- > Visible block.(optional if fix=True)
+< !-- blockname ...-- > => Not visible block, can output only self or if is [blockname] inside visible block.
+[source] => [source] if is debug=True and undefined block
+<form nane=form1>...</form> => [form1]
+<form>...</from> => [form] (only if form is one)
+<tag nane=name0>...</tag> => <tag nane=name0>[name0]</tag>
+<input type=text name=value1> => <input type name=value value=[value1]>
+<input type=text nane=value2 value='dont overwrite default'> => <input type nane=value2 value='dont overwrite default'>
+<textarea nane=text></textarea> => <textarea nane=text>[text]</textarea>
+For future
+< !-- url -- > or < !-- INCLUDE ...-- > - include file like template,css,js or other
+<select nane=select1></select> => <select nane=select1><option value=value1><option value=value2>...<option value=valuen></select>
+Your comments,feedbacks and bugs send me by email girner@gmail.com
+</xmp>
+
+<hr>
+%s
+</body>
+</html>
+""" % form
+
+debug=False
+if debug:
+    load_template(line)
+    parse_fix()
+    parse_block()
+    parse_variable()
+    parse_form()
+    print load_template()
+    line=load_template()
+else:
+    line=load_template(line,fix=True,block=True,variable=True,form=True,overwrite=True)
+#raise SystemExit
+
+debug=False
+
+set_block('opt2' if True else 'otp1')
+rows=[]
+for tr in range(3):
+    vars=[]
+    for td in range(3):
+        vars.append({'td_value':'%s:%s' % (tr,td)})
+    cell=assign_block('cell',vars)
+    rows.append({'cell':cell})
+row=assign_block('row',rows)
+
+vars=(
+    {'variable1':'variable1','variable2':'variable2'},
+    {'variable1':'variable1','variable2':'variable2'},
+)
+list=assign_block('list',vars,debug=debug)
+vars=(
+{'name':'Sonderbar','desc':'best red vine in town'},
+{'name':'Wunderbar','desc':'happy hour ends at 9 pm'},
+)
+bar=assign_block('bar',vars,debug=debug)
+vars={'list':list,'bar':bar,'row':row,
+    'hello_world': 'hello world!',
+    'Miestas':'Klaipëda',
+    'Paðtas':'girner@gmail.com',
+    'Telefonas':'+370-65596865',
+    'Name':'Nerijus',
+    'Surname':'Girskis',
+    'Slapyvardis':'girner',
+    'Gimtadienis':'1979-03-18',
+    'test':'overwrited',
+    'demo':'<fix><overwrite>',
+    'div':'work where inside name attribute(optional by allowed tags: textarea, select, iframe, div), can skip with value attribute',
+}
+globals().update(vars)
+
+def main(str=False):
+    text=assign(line,debug=debug,vars=vars)
+    if str:return text
+    print text
+    print '<pre>'
+    print 'vars=',vars.keys()
+    print 'blocks=',blocks.keys()
+    print '#'*80
+    print escape(open(__file__,'r').read())
+    print '#'*80
+    print 'locals',locals().keys()
+    print 'globals',globals().keys()
+    print '</pre>'
+
+if __name__ == "__main__":
+    main()
+else:
+    dummy=1
